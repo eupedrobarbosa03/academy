85576a595434bf5f256661e409d7961391dc09f9
Pedro <eupedrobarbosa03@gmail.com>
2026-04-19 13:58:06 -0300

    aplicando responsividade mobile menu

---
+++ dist/js/menu-mobile.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector("#menu-navbar");
+const navbar = document.querySelector(".navbar-container-options");
+menu.addEventListener("click", () => {
+    navbar.classList.toggle("show");
+});
+;
+export {};
---
+++ src/menu-mobile.ts
@@ -0,0 +1,6 @@
+const menu = document.querySelector("#menu-navbar") as HTMLDivElement;
+const navbar = document.querySelector(".navbar-container-options") as HTMLDivElement;
+
+menu.addEventListener("click", () => {
+    navbar.classList.toggle("show")
+});;
