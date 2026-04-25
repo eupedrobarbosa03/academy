466461a3c107320cfda07070d553e765bf34b022
Pedro <eupedrobarbosa03@gmail.com>
2026-04-24 22:02:30 -0300

    adicionando ícones nos botões de navegação. Funcionalide mobile: clicar no botão do navbar, navbar some

--- src/menu-responsive.ts
+++ src/menu-responsive.ts
@@ -1,14 +1,20 @@
 class Menu {
     private menu: HTMLDivElement;
     private navbar: HTMLDivElement;
+    private buttonsOption;
     constructor() {
         this.menu = document.querySelector("#menu-navbar") as HTMLDivElement;
         this.navbar = document.querySelector(".navbar-container-options") as HTMLDivElement;
+        this.buttonsOption = document.querySelectorAll<HTMLButtonElement>(".button-option");
     };
 
     responsive() {
         this.menu.addEventListener("click", () => this.navbar.classList.toggle("show"));
+        this.buttonsOption.forEach((button) => button.addEventListener("click", (e) => {
+            if (this.navbar.classList.contains("show")) this.navbar.classList.remove("show");
+        }));
     };
+
 };
 
 export const menu = new Menu();
