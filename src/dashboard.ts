376e17d99b87ef357b543862c5ffd9fcddf51c5a
Pedro <eupedrobarbosa03@gmail.com>
2026-05-01 10:10:56 -0300

    substituindo number por +

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -16,24 +16,24 @@ class Dashboard {
     update(type: "create" | "delete" | "conclude" | "cancel") {
         return {
             students: () => {
-                const currentNumber = Number(this.students.textContent);
+                const currentNumber = +this.students.textContent;
                 if (type === "create") {
                     this.students.textContent = `${currentNumber + 1}`;     
                     return;
                 }; this.students.textContent = `${currentNumber - 1}`
             },
             instructors: () => {
-                const currentNumber = Number(this.instructors.textContent);
+                const currentNumber = +this.instructors.textContent;
                 if (type === "create") {
                     this.instructors.textContent = `${currentNumber + 1}`;     
                     return;
                 }; this.instructors.textContent = `${currentNumber - 1}`  
             },
             workouts: () => {
-                const currentNumberWorkoutsConclude = Number(this.concludeWorkouts.textContent)
-                const currentNumberWorkoutsCanceled = Number(this.canceledWorkouts.textContent)
+                const currentNumberWorkoutsConclude = +this.concludeWorkouts.textContent
+                const currentNumberWorkoutsCanceled = +this.canceledWorkouts.textContent
                 if (type === "create") {
-                    const currentNumber = Number(this.workouts.textContent);
+                    const currentNumber = +this.workouts.textContent;
                     this.workouts.textContent = `${currentNumber + 1}`;
                 } else if (type === "conclude") {
                     this.concludeWorkouts.textContent = `${currentNumberWorkoutsConclude + 1}`
